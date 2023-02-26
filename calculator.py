f811cdc80bc117d8ed0ca7977b2dfe86dfb18630
Krzysztof Wojcikiewicz <krzysztof.wojcikiewicz@gmail.com>
2023-02-27 00:34:00 +0100

    Add and implement employment calculator with GUI

--- calculator.py
+++ calculator.py
@@ -42,3 +42,29 @@ def calculate_mandate():
         layout.vnetamount.set("{:.2f}".format(netamount)+" PLN")
     except ValueError:
         messagebox.showerror("Value error", "Error: You can only input numbers!\nUse full stop as a separator as such: 0.00")
+
+
+def calculate_employment():
+
+    try:
+        pensionamount = round(round(float(layout.vgrossamount.get()), 2) * 0.0976, 2)
+        disamount = float(layout.vgrossamount.get()) * 0.015
+        sicamount = float(layout.vgrossamount.get()) * 0.0245
+        capitalamount = float(layout.vgrossamount.get()) * 0.02 * layout.vppk.get()
+        capitaltax = float(layout.vgrossamount.get()) * 0.015 * layout.vppk.get()
+        healthamount = round(round((float(layout.vgrossamount.get()) - (pensionamount + disamount + sicamount)), 2) * 0.09 * layout.vstud.get(), 2)
+        taxexpenence = ((float(layout.vgrossamount.get()) + float(capitaltax)) - (pensionamount + disamount + sicamount)) * float(layout.vtaxexpenses.get())
+        taxbaseamount = round((float(layout.vgrossamount.get()) + capitaltax) - (pensionamount + disamount + sicamount + taxexpenence), 0)
+
+        taxamount = taxbaseamount * float(layout.vtaxbracket.get()) * layout.vage.get() * layout.vstud.get()
+        netamount = float(layout.vgrossamount.get()) - (round(taxamount, 0) + round(pensionamount, 2) + round(disamount, 2) + round(sicamount, 2) + round(capitalamount, 2) + round(healthamount, 2))
+
+        layout.vpensionamount.set("{:.2f}".format(round(pensionamount, 2)) + " PLN")
+        layout.vdisamount.set("{:.2f}".format(round(disamount, 2)) + " PLN")
+        layout.vsicamount.set("{:.2f}".format(round(sicamount, 2)) + " PLN")
+        layout.vcapitalamount.set("{:.2f}".format(round(capitalamount, 2)) + " PLN")
+        layout.vhealthamount.set("{:.2f}".format(round(healthamount, 2))+" PLN")
+        layout.vtaxamount.set("{:.2f}".format(round(taxamount, 0))+" PLN")
+        layout.vnetamount.set("{:.2f}".format(netamount)+" PLN")
+    except ValueError:
+        messagebox.showerror("Value error", "Error: You can only input numbers!\nUse full stop as a separator as such: 0.00")
--- layout.py
+++ layout.py
@@ -94,7 +94,7 @@ button1 = ttk.Button(top_frame, text="Contract for\nspecific work", command=butt
 button1.grid(row=0, column=0)
 button2 = ttk.Button(top_frame, text="Contract of\nmandate", command=button2, width=20)
 button2.grid(row=0, column=1)
-button3 = ttk.Button(top_frame, text="Employment\n", command=button3, width=20, state=DISABLED)
+button3 = ttk.Button(top_frame, text="Employment\n", command=button3, width=20)
 button3.grid(row=0, column=2)
 button4 = ttk.Button(top_frame, text="Self-employment\n", command=button4, width=20, state=DISABLED)
 button4.grid(row=0, column=3)
@@ -172,8 +172,17 @@ bottom_frame3 = ttk.Frame(root)
 ttk.Label(bottom_frame3, text="Enter gross amount:", justify="left", width=35).grid(column=0, row=0)
 ttk.Label(bottom_frame3, text="Pick your tax bracket:", justify="left", width=35).grid(column=0, row=1)
 ttk.Label(bottom_frame3, text="Pick your tax deductible expenses:", justify="left", width=35).grid(column=0, row=2)
-ttk.Label(bottom_frame3, text="Taxes:", justify="left", width=35).grid(column=0, row=3)
-ttk.Label(bottom_frame3, text="Net pay:", justify="left", width=35).grid(column=0, row=4)
+ttk.Label(bottom_frame3, text="Student under the age of 26?", justify="left", width=35).grid(column=0, row=3)
+ttk.Label(bottom_frame3, text="Person under the age of 26?", justify="left", width=35).grid(column=0, row=4)
+ttk.Label(bottom_frame3, text="Voluntary Employee Capital Plans?", justify="left", width=35).grid(column=0, row=7)
+
+ttk.Label(bottom_frame3, text="Old age pension insurance:", justify="left", width=35).grid(column=0, row=8)
+ttk.Label(bottom_frame3, text="Disability pension insurance:", justify="left", width=35).grid(column=0, row=9)
+ttk.Label(bottom_frame3, text="Sickness allowance insurance:", justify="left", width=35).grid(column=0, row=10)
+ttk.Label(bottom_frame3, text="Employee Capital Plans:", justify="left", width=35).grid(column=0, row=11)
+ttk.Label(bottom_frame3, text="Health insurance:", justify="left", width=35).grid(column=0, row=12)
+ttk.Label(bottom_frame3, text="Taxes:", justify="left", width=35).grid(column=0, row=13)
+ttk.Label(bottom_frame3, text="Net pay:", justify="left", width=35).grid(column=0, row=14)
 
 ttk.Entry(bottom_frame3, textvariable=vgrossamount, justify="right").grid(column=1, row=0, columnspan=2)
 
@@ -183,10 +192,19 @@ ttk.Radiobutton(bottom_frame3, text="32%", variable=vtaxbracket, value="0.32").g
 ttk.Radiobutton(bottom_frame3, text="20%", variable=vtaxexpenses, value="0.20").grid(column=1, row=2)
 ttk.Radiobutton(bottom_frame3, text="50%", variable=vtaxexpenses, value="0.50").grid(column=2, row=2)
 
-ttk.Label(bottom_frame3, textvariable=vtaxamount, justify="right").grid(column=2, row=3)
-ttk.Label(bottom_frame3, textvariable=vnetamount, justify="right").grid(column=2, row=4)
+ttk.Checkbutton(bottom_frame3, text="Yes", variable=vstud, onvalue=0, offvalue=1).grid(column=2, row=3)
+ttk.Checkbutton(bottom_frame3, text="Yes", variable=vage, onvalue=0, offvalue=1).grid(column=2, row=4)
+ttk.Checkbutton(bottom_frame3, text="Yes", variable=vppk).grid(column=2, row=7)
 
-# ttk.Button(bottom_frame3, text="Calculate", command=calculator.calculate).grid(column=4, row=0)
+ttk.Label(bottom_frame3, textvariable=vpensionamount, justify="right").grid(column=2, row=8)
+ttk.Label(bottom_frame3, textvariable=vdisamount, justify="right").grid(column=2, row=9)
+ttk.Label(bottom_frame3, textvariable=vsicamount, justify="right").grid(column=2, row=10)
+ttk.Label(bottom_frame3, textvariable=vcapitalamount, justify="right").grid(column=2, row=11)
+ttk.Label(bottom_frame3, textvariable=vhealthamount, justify="right").grid(column=2, row=12)
+ttk.Label(bottom_frame3, textvariable=vtaxamount, justify="right").grid(column=2, row=13)
+ttk.Label(bottom_frame3, textvariable=vnetamount, justify="right").grid(column=2, row=14)
+
+ttk.Button(bottom_frame3, text="Calculate", command=calculator.calculate_employment).grid(column=4, row=0)
 
 
 bottom_frame4 = ttk.Frame(root)
